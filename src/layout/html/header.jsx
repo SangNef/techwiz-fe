90dafc891e4249179b2aa8cccf13fe3d671a5547
zhong xina <117057017+thai4356@users.noreply.github.com>
2024-09-18 11:14:34 +0700

    header

---
+++ src/layout/html/header.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Helmet } from 'react-helmet';
+
+function Header() {
+    return (
+        <>
+            <Helmet>
+                <link rel="stylesheet" href="../css/1.css" />
+                <link rel="stylesheet" href="../css/2.css" />
+                <link rel="stylesheet" href="../css/3.css" />
+                <link rel="stylesheet" href="../css/4.css" />
+                <link rel="stylesheet" href="../css/5.css" />
+                <link rel="stylesheet" href="../css/6.css" />
+                <link rel="stylesheet" href="../css/animation.css" />
+                <link rel="stylesheet" href="../css/=.min" />
+        
+            </Helmet>
+          </>
+    );
+}
+
+export default Header;
+
